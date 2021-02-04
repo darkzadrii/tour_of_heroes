08094230c59d0743948c9c488f6d11f9392eda47
Adrianne Paul Lagleva <adriannepaullagleva@yahoo.com>
2021-02-04 16:59:47 +0100

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-/***/
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+/**è il centro smistamento, in base a cosa ho nella barra url, va da qualche parte*/
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
 ];
 
 @NgModule({
